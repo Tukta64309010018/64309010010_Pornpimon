c2a92ed08704bad29edfdde6d77b831174a2d333
Chanapa Siriphan <chanapasiriphan456@gmail.com>
2022-11-10 13:55:22 +0700

    Create TestJS.js

---
+++ week4/TestJS.js
@@ -0,0 +1,12 @@
+console.log(5);
+var myName = 'Pornpimon';
+let lastName = 'Kachan';
+
+const year = 2002;
+//year = 2023;
+
+let grade = 3.56;
+grade = Math.floor(grade);
+console.log(grade);
+
+console.log(`My Name is ${myName}`);
